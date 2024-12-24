5b144e21541cc0567a16ff1dbf3f982c078700a1
elkolesnikova <elkolesnkova@neoflex.ru>
2024-12-24 18:30:49 +0300

    Initial commit

---
+++ src/main/java/ru/neoflex/education/camunda/PrepareToBattle.java
@@ -0,0 +1,39 @@
+package ru.neoflex.education.camunda;
+
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PrepareToBattle implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        int warriors = (int) delegateExecution.getVariable("warriors");
+        int enemyWarriors = (int) (Math.random() * 100);
+        String battleStatus = "Undefined";
+        boolean isWin = false;
+
+
+        if ((warriors - enemyWarriors) > 0) {
+            isWin = true;
+
+            battleStatus = "Victory!";
+
+        } else {
+            battleStatus = "Fail :(";
+
+        }
+
+        if (warriors < 1 || warriors > 100) {
+            throw new BpmnError("warriorsError");
+        }
+
+
+        delegateExecution.setVariable("enemyWarriors", enemyWarriors);
+        delegateExecution.setVariable("battleStatus", battleStatus);
+        delegateExecution.setVariable("isWin", isWin);
+    }
+
+}
